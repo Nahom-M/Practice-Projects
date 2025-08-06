40a0e48f6b32a531c40d1fc1f85caa6aa8ae8579
Nahom Mesele <nahommese@gmail.com>
2025-08-05 21:52:50 -0400

    fixed header image size

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <img
         src="/nahom-picture.jpg"
         alt="Profile"
-        className="mx-auto w-100 h-100 rounded-full object-cover"
+        className="mx-auto w-100 h-100 rounded-full object-cover border-10 border-black-300"
       />
       <div className="mt-5 text-2xl font-medium">
         Hello, my name is Nahom Mesele and I'm a recent Humber graduate from
