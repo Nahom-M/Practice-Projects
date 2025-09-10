114c99df9d5f345bd089b5aac6a394b1cd8bdd6e
Nahom Mesele <nahommese@gmail.com>
2025-09-10 15:58:57 -0400

    added starting pages

--- web-lessons/app/page.tsx
+++ web-lessons/app/page.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1 className="text-3xl font-bold">
+      <h1 className="text-6xl font-bold mt-20">
         Welcome to Next.js!
       </h1>
-      <p>A platform made to help teach those who aren't up to date with current times.</p>
+      <p className='mt-5 mb-20'>A platform made to help teach those who aren't up to date with current times.</p>
 
-      <h3 className="text-2xl font-bold mt-4">
+      <h3 className="text-6xl font-bold mb-15">
         Lessons
       </h3>
+      
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
+        <div>Gmail</div>
+        <div>Internet</div>
+        <div>Google Drive</div>
+      </div>
     </div>
   );
 }
